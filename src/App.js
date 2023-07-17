0471b66a141e570f8128b36ef1f833fc0f359929
aamuhammadi <aamconsultants17@gmail.com>
2023-07-17 20:00:43 +0500

    Class 10

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
-import Navbar from "./Components/Nav/navbar";
-import "./style.css";
+// import Navbar from "./Components/Nav/navbar";
 import BarberShop from "./Components/barberShopCode";
 
 function App() {
-  const [wives, setWives] = useState(0);
+  // const [wives, setWives] = useState(0);
 
-  const [kids, setKids] = useState(0);
+  // const [kids, setKids] = useState(0);
 
-  const addWife = () => {
-    setWives((prevWives) => prevWives + 1);
-    setKids((prevKids) => prevKids + 3);
-  };
+  // const addWife = () => {
+  //   setWives((prevWives) => prevWives + 1);
+  //   setKids((prevKids) => prevKids + 3);
+  // };
 
-  const minWife = () => {
-    if (wives > 0) {
-      setWives(wives - 1);
-    }
-  };
+  // const minWife = () => {
+  //   if (wives > 0) {
+  //     setWives(wives - 1);
+  //   }
+  // };
 
-  const resetValue = () => {
-    setWives(0);
-    setKids(0);
-  };
+  // const resetValue = () => {
+  //   setWives(0);
+  //   setKids(0);
+  // };
 
   return (
     <div className="App">
