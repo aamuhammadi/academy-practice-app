f22678218833b29463a6dc676d27513046c7003d
aamuhammadi <aamconsultants17@gmail.com>
2023-08-07 20:15:46 +0500

    Class 20

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
-// import Navbar from "./Components/Nav/navbar";
-import BarberShop from "./Components/barberShopCode";
+import React, { useCallback, useState } from "react";
+import "./App.css";
 
 function App() {
-  const MainHeading = "Hair Studio";
-  const SubHeading = "Style Your Hair is Style Your Life";
+  const [newTask, setNewTask] = useState("");
+  const [pendingTasks, setPendingTasks] = useState([]);
 
+  const addPendingTask = () => {
+    setPendingTasks([...pendingTasks, newTask]);
+    setNewTask("");
+  };
+
+  const handleAddTask = (e) => {
+    setNewTask(e.target.value);
+  };
   return (
     <div className="App">
-      <BarberShop MainHeading={MainHeading} SubHeading={SubHeading} />
+      <h1>To Do List App</h1>
+      <input
+        type="text"
+        placeholder="Add task"
+        value={newTask}
+        onChange={handleAddTask}
+      ></input>
+      <button onClick={addPendingTask}>Add Task</button>
+      <h3>Pending Tasks</h3>
+      <ul>
+        {pendingTasks.map((pt, i) => (
+          <li key={i}>{pt}</li>
+        ))}
+      </ul>
     </div>
   );
 }
