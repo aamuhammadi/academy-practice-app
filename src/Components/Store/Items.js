2638d899125635ee51a59590c735451da500015b
aamuhammadi <aamconsultants17@gmail.com>
2023-08-24 19:21:33 +0500

    class 29

--- src/Components/Store/Items.js
+++ src/Components/Store/Items.js
@@ -8,20 +8,47 @@ function Items() {
     { id: 3, name: "mobile", price: 24500 },
     { id: 4, name: "laptop", price: 35000 },
     { id: 5, name: "microphone", price: 750 },
+    // { id: 6, name: "chair", price: 1000 },
   ];
 
+  const newProduct = "";
+
+  // const productName = () => {
+  //   if (newProduct === "table") {
+  //     console.log("Office items");
+  //   } else if (newProduct === "") {
+  //     console.log("Product not found");
+  //   } else {
+  //     console.log("This is not office item");
+  //   }
+  // };
+
+  let productsList;
+
+  if (products.length < 6) {
+    productsList = "Products are less than 6";
+  } else {
+    productsList = products.map((prod) => (
+      <ul>
+        <li key={prod.id}>
+          <Link to={`/items/${prod.id}`}>
+            {prod.name} | {prod.price}
+          </Link>
+        </li>
+      </ul>
+    ));
+  }
+
+  const ternaryOptTest = () => {
+    products.length < 5
+      ? console.log("products are less than 5")
+      : console.log(products[1].name);
+  };
+  ternaryOptTest();
   return (
     <div>
       <h1>Items</h1>
-      {products.map((prod) => (
-        <ul>
-          <li>
-            <Link to={`/items/${prod.id}`}>
-              {prod.name} | {prod.price}
-            </Link>
-          </li>
-        </ul>
-      ))}
+      {productsList}
     </div>
   );
 }
